b89f1c8b1a7364dfda3e54eb6c2147f356edab27
smartin <smartin@gigrowrevolution.com>
2018-02-26 13:35:33 -0500

    Add a way to specify a different servlet for the vaadin registration

--- vaadin7/src/main/groovy/grails/plugins/vaadin/server/GrailsAwareVaadinServletRegistrationBean.groovy
+++ vaadin7/src/main/groovy/grails/plugins/vaadin/server/GrailsAwareVaadinServletRegistrationBean.groovy
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.context.embedded.ServletRegistrationBean
 import org.vaadin.grails.navigator.UriMappings
 
+import javax.servlet.Servlet
 import javax.servlet.ServletRegistration
 
 /**
@@ -23,7 +24,11 @@ class GrailsAwareVaadinServletRegistrationBean extends ServletRegistrationBean {
     UriMappings uriMappings
 
     GrailsAwareVaadinServletRegistrationBean() {
-        super(new GrailsAwareVaadinServlet(), ['/VAADIN/*'] as String[])
+        this(new GrailsAwareVaadinServlet())
+    }
+
+    GrailsAwareVaadinServletRegistrationBean(Servlet servlet) {
+        super(servlet, ['/VAADIN/*'] as String[])
     }
 
     protected String getUIProviderClassName() {
